f9b3d0a38b130016d57c9a03770e8deba5ff90a3
Feihong <feihong.hsu@gmail.com>
2016-12-28 18:47:34 -0600

    Use much longer promise chain

--- promise-priority.js
+++ promise-priority.js
@@ -1,25 +1,25 @@
 /*
 Show that promise chaining takes priority over setImmediate().
 
+Note that calling setImmediate() once in the middle of the chain will allow the
+first setImmediate() callback to run before the chain finishes.
 */
 
 setImmediate(() => {
+  // You won't see this until the rest of the program has completed.
   console.log('Inside setImmediate callback')
 })
 
-Promise.resolve(1)
-.then(value => {
-  console.log(value)
-  return 2
-})
-.then(value => {
-  console.log(value)
-  return 3
-})
-.then(value => {
-  console.log(value)
-  return 42
-})
-.then(value => {
+let promise = Promise.resolve(1)
+for (let i=2; i < 1000; i++) {
+  promise = promise.then(value => {
+    console.log(value)
+    // if (i === 970) {
+    //   return new Promise(resolve => setImmediate(resolve, i))
+    // }
+    return i
+  })
+}
+promise.then(value => {
   console.log(`Final value ${value}`)
 })

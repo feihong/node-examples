1d7739fb55cd7188b371833ade0b5df2512bb44f
Feihong <feihong.hsu@gmail.com>
2016-12-29 19:16:04 -0600

    Add FourSquare API example

---
+++ foursquare.js
@@ -0,0 +1,30 @@
+require('isomorphic-fetch')
+const qs = require('querystring')
+
+let [client_id, client_secret] = process.env.FOURSQUARE_PARAMS.split(';')
+let params = {
+  client_id,
+  client_secret,
+  v: '20161230',
+  ll: '41.967985,-87.688307',
+}
+let url = 'https://api.foursquare.com/v2/venues/search?' + qs.stringify(params)
+// console.log(url)
+fetch(url)
+.then(res => res.json())
+.then(data => {
+  let venues = data.response.venues
+
+  // printVenue(venues[0])
+  for (let venue of venues) {
+    printVenue(venue)
+    console.log('===============')
+  }
+})
+
+function printVenue(v) {
+  console.log(v.name)
+  console.log(v.location.address)
+  let categories = v.categories.map(c => c.name)
+  console.log(categories.join(','))
+}

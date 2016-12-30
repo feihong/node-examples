0ba85418faff7e0fb1405b9fee5b9f12ad4429c8
Feihong Hsu <feihong.hsu@gmail.com>
2016-12-30 10:20:20 -0600

    Example of using Gracenote API's showings endpoint

--- gracenote-api.js
+++ gracenote-api.js
@@ -1,14 +1,18 @@
 require('isomorphic-fetch')
 const moment = require('moment')
 const qs = require('querystring')
+const baseUrl = 'http://data.tmsapi.com/v1.1/movies'
+const api_key = process.env.GRACENOTE_API_KEY
 
 
-function getTheaters(zipcode) {
+function getShowings(lat, lng) {
   let params = {
-    zip: zipcode,
-    api_key: process.env.GRACENOTE_API_KEY
+    startDate: moment().format('YYYY-MM-DD'),
+    lat,
+    lng,
+    api_key
   }
-  let url = 'http://data.tmsapi.com/v1.1/theatres?' + qs.stringify(params)
+  let url = baseUrl + '/showings?' + qs.stringify(params)
   console.log(url)
   return fetch(url)
   .then(res => {
@@ -20,6 +24,35 @@ function getTheaters(zipcode) {
   })
 }
 
-getTheaters(60625)
-.then(data => console.log(data))
-.catch(err => console.log(err))
+function getTheaters(zip) {
+  let params = {
+    zip,
+    api_key
+  }
+  let url = baseUrl + '/theatres?' + qs.stringify(params)
+  console.log(url)
+  return fetch(url)
+  .then(res => {
+    if (res.status == 200) {
+      return res.json()
+    } else {
+      return Promise.reject(`Received status code ${res.status}: ${res.headers.get('x-error-detail-header')}`)
+    }
+  })
+}
+
+// Unfortunately, the free plan does not give you access to the theaters endpoint.
+// getTheaters(60625)
+// .then(data => console.log(data))
+// .catch(err => console.log(err))
+
+getShowings(41.967985, -87.688307)
+.then(data => {
+  // console.log(data))
+  for (let movie of data) {
+    console.log(movie.title)
+    console.log(movie.releaseYear)
+    console.log(movie.genres.join(', '))
+    console.log('=============')
+  }
+})

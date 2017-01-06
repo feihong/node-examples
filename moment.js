96e7031ae32b320d8297c49ab146903da31824c4
Feihong Hsu <feihong.hsu@gmail.com>
2017-01-05 22:24:19 -0600

    Add an example of parsing a date time string

--- moment.js
+++ moment.js
@@ -1,4 +1,12 @@
 const moment = require('moment')
 
+// Format a date object.
 let now = moment()
 console.log(now.format('YYYY-MM-DD'))
+
+// Parse a date time string.
+let dt = moment('2017-01-05T20:15')
+console.log(dt)
+
+// Print out just the time.
+console.log(dt.format('hh:mm A'))
